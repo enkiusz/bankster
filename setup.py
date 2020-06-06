79998ed5d32fe7df396273e5805d2f0c7b863406
Maciej Grela <enki@fsck.pl>
2020-06-07 00:54:20 +0200

    bankster: Initial revision copied from greatriver

---
+++ setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="bankster", # Replace with your own username
+    version="0.0.1",
+    author="Maciej Grela",
+    author_email="enki@fsck.pl",
+    description="Access information from financial institutions",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/mgrela/bankster",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+        "Development Status :: 3 - Alpha",
+        "Topic :: Office/Business :: Financial"
+    ],
+    python_requires='>=3.7',
+)
